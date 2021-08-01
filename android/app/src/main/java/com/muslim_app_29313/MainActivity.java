eb9361e77c8ff290ac1dca3549f82b381344f225
Crowdbotics <team@crowdbotics.com>
2021-08-01 10:28:21 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/muslim_app_29313/MainActivity.java
@@ -0,0 +1,15 @@
+package com.muslim_app_29313;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "muslim_app_29313";
+  }
+}
